74756d4f93aa936588bf3a17a403f68954a56b24
fzrain <250970574@qq.com>
2015-08-17 22:19:31 +0800

    fix bug

--- Fzrain.Web/Views/userManager/auditLog.js
+++ Fzrain.Web/Views/userManager/auditLog.js
@@ -1,22 +1,30 @@
 ﻿(function () {
     angular.module('materialAdmin').controller('auditController', [
-       'ngTableParams', 'abp.services.app.auditLog',
+       'ngTableParams', 'abp.services.app.auditLog','param',
         function (ngTableParams,auditService) {          
             var vm = this;
             vm.auditLogs = [];
             vm.pageSize = 10;
-            vm.pageIndex = 1;         
-            vm.loadData = function (pageIndex) {             
-                  abp.ui.setBusy(
-                  null,
-                  auditService.getAuditLogs({
-                      skipCount: (pageIndex - 1) * vm.pageSize
-                  }).success(function (data) {
-                      vm.auditLogs = data.items;
-                      vm.total = data.totalCount;                    
-                  })
-              );
-                             
+            vm.pageIndex = 1;
+           
+            vm.loadData = function (pageIndex) {
+                auditService.getAuditLogs({
+                    skipCount: (pageIndex - 1) * vm.pageSize
+                }).success(function(data) {
+                    vm.auditLogs = data.items;
+                    vm.total = data.totalCount;
+                    vm.tableBasic = new ngTableParams({
+                        page: vm.pageIndex, // show first page
+                        count: vm.pageSize // count per page
+                    }, {
+                        total: vm.total, // length of data
+                        getData: function () {
+                            return vm.auditLogs;
+
+                        }
+
+                    });
+                });
             }
             vm.getAuditDetail= function(id) {
                 auditService.getDetail({ id: id }).success(function(data) {
@@ -24,16 +32,7 @@
                 });
             }
             vm.loadData(1);
-            this.tableBasic = new ngTableParams({
-                page: vm.pageIndex, // show first page
-                count: vm.pageSize // count per page
-            }, {
-                total: vm.total, // length of data
-                getData: function($defer, params) {
-                    $defer.resolve(vm.loadData(params.page()));
-                }
-              
-            });
+         
         
           
         }
